07c1affaa8afed82e0346133d1835b298c44425c
diego <diego@diegobm>
2018-03-29 11:59:50 -0600

    second commit se agregan las imagenes

--- src/numerosromanostec/NumerosRomanosTec.java
+++ src/numerosromanostec/NumerosRomanosTec.java
@@ -9,56 +9,39 @@ import java.util.Scanner;
 
 public class NumerosRomanosTec {
 
+    public static int[] numero = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    public static String[] numRoman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-
-public static int [] numero={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-public static String [] numRoman={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         String entradaTeclado = "";
         Scanner entradaEscaner = new Scanner(System.in);
         int i;
-        while(true){
+        while (true) {
             entradaTeclado = entradaEscaner.nextLine();
-            if(getValidate(entradaTeclado))
-            break;
-            int entrada=Integer.parseInt(entradaTeclado);
-            if (entrada==0) {
+
+            int entrada = Integer.parseInt(entradaTeclado);
+            if (entrada == 0) {
                 break;
-            }else{
-                System.out.printf("%-4d",entrada);
-             i=0;
-                while(entrada>0){
-                    if(entrada>=numero[i]){
+            } else {
+                System.out.printf("%-4d", entrada);
+                i = 0;
+                while (entrada > 0 && entrada <= 1000) {
+                    if (entrada >= numero[i]) {
                         System.out.print(numRoman[i]);
-                        entrada=entrada-numero[i];
-                    }else{
+                        entrada = entrada - numero[i];
+                    } else {
                         i++;
                     }
                 }
                 System.out.println("");
             }
-            
-            
+
         }
-        
-    }
-    
-    public static boolean getValidate(String valor){
-        try {
-            int numero=Integer.parseInt(valor);
-            if(numero<=1000 || numero>=1){
-                return true;
-            }else
-            return false;
-        } catch (Exception e) {
-            System.err.println("ocurrio un error !");
-            return true;
-        }
+
     }
 
 }
